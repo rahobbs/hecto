335eca5027c1af8048454c8831c25d3bda3fb7af
rahobbs <hobbs.rachael@gmail.com>
2021-02-21 15:10:04 -0500

    next up, ch3 tildes

---
+++ src/editor.rs
@@ -0,0 +1,60 @@
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;            
+use termion::raw::IntoRawMode;
+
+pub struct Editor {
+    should_quit: bool,
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        Self { should_quit: false }
+    }
+
+    fn refresh_screen(&self) -> std::io::Result<()> {
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye.\r");
+        }
+        io::stdout().flush()
+    }
+
+    fn process_keypress(&mut self) -> std::io::Result<()> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+}
+
+fn read_key() -> std::io::Result<Key> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
+    panic!(e);
+}
